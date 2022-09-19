fd908e442144201a11bdb4a83d2f54c4d77570e4
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-19 21:36:10 +0400

    сортировка linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([12, 43, 14, 64, 34, 47, 97, 93, 41, 54, 32, 17, 36, 4, 39, 66, 86, 9, 95, 94])
 const characterCollection = new CharacterCollection('qwerTyuiopasDfghjklZxcvbnm');
+
+const linkedList = new LinkedList();
+linkedList.add(12);
+linkedList.add(43);
+linkedList.add(14);
+linkedList.add(64);
+linkedList.add(34);
+linkedList.add(47);
+
 const sorter = new Sorter(numbersCollection);
 const sorter2 = new Sorter(characterCollection);
+const sorter3 = new Sorter(linkedList);
 
 sorter.sort();
 sorter2.sort();
+sorter3.sort();
 
 console.log(numbersCollection.data);
 console.log(characterCollection.data);
+linkedList.print();
